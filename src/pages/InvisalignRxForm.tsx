04c379f5b30fd601474cb35ca1d60761aebd7e9b
1-arjayabalan-0 <arjayabalan09@gmail.com>
2026-02-23 22:09:02 +0530

    alias,redirect fixes--3

--- src/pages/InvisalignRxForm.tsx
+++ src/pages/InvisalignRxForm.tsx
@@ -77,7 +77,7 @@ function DentalChartSelection({
                 </div>
                 <div className="h-[1px] w-full bg-gray-100 lg:hidden" id="mob-border" style={{
                     position: "relative",
-                    top: "-8px"
+                    top: "-14px"
                 }}></div>
                 <div className="space-y-6 min-w-max flex lg:flex-row md:flex-row">
                     <div className="border-r pr-1 sm:mb-6">
